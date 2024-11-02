3c5447ea4c4bce57f47f7628b944647cedf982eb
Angelarose-19 <angelitarosero774@gmail.com>
2024-11-01 19:12:38 -0500

    na

---
+++ ejercicios/proyectodeprueba/Tareas/reto.js
@@ -0,0 +1,5 @@
+
+
+^e "" `o ^*/a`e "" ^u`-o "" #e$ "" /o "" %iee "" que""e{=oy "" =$a%a!a]¡u "" y"" ` ó ^ o "" e{=o""au^ee=a "" ^i{ "" *o¡e$e{ "" ¡e "" a=ee`i ó ] "" y "" e{;ue$__o "" `o]=i]uo
+
+    g, k, w, x                                              
